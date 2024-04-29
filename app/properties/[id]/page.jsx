196471ae51e34e3c9c6cd8e870fbdbcd20a1f6e8
Helene <40670839+Bertiscoding@users.noreply.github.com>
2024-04-29 18:19:23 +0200

    property details

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,12 +1,80 @@
-import Link from 'next/link'
+'use client';
+import { useState, useEffect } from "react";
+import { useParams } from "next/navigation";
+import { fetchProperty } from "@/utils/requests";
+import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+import PropertyDetails from "@/components/PropertyDetails";
+import Link from "next/link";
+import Spinner from "@/components/Spinner";
+import { FaArrowLeft } from 'react-icons/fa';
 
-const PropertiesPage = () => {
+const PropertyPage = () => {
+  const { id } = useParams();
+
+  const [property, setProperty] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPropertyData = async () => {
+      if (!id) return;
+      try {
+        const property = await fetchProperty(id);
+        setProperty(property);
+      } catch (error) {
+        console.error('Error fetching property:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (property === null) {
+      fetchPropertyData();
+    }
+  }, [id, property]);
+
+  if (!property && !loading) {
+    return (
+      <h1 classNameName='text-center text-2xl font-bold mt-10'>
+        Property Not Found
+      </h1>
+    );
+  }
+
   return (
-    <div>
-      <h1 className="text-3xl">PropertiesPage</h1>
-      <Link href="/">Go Home</Link>
-    </div>
+    <>
+    { loading && <Spinner loading={loading}/> }
+    {!loading && property && (
+      <>
+        <PropertyHeaderImage image={property.images[0]} />
+        <section>
+          <div className="container m-auto py-6 px-6">
+            <Link
+              href="/properties"
+              className="text-blue-500 hover:text-blue-600 flex items-center"
+            >
+              <FaArrowLeft className='mr-2' /> Back to Properties
+            </Link>
+          </div>
+        </section>
+
+
+        <section className='bg-blue-50'>
+            <div className='container m-auto py-10 px-6'>
+              <div className='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
+                <PropertyDetails property={property} />
+                <div>PropertyDetails</div>
+                <aside className='space-y-4'>
+                <div>BookmarkButton</div>
+                <div>ShareButtons</div>
+                <div>PropertyContactForm</div>
+                </aside>
+              </div>
+            </div>
+          </section>
+      </>
+    )}
+    </>
   )
 }
 
-export default PropertiesPage
+export default PropertyPage
--- utils/requests.js
+++ utils/requests.js
@@ -1,5 +1,6 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null; // to avoid errors on deployment pipeline
 
+// Fetch all properties
 async function fetchProperties() { // doing it like this because its a server-side component
   try {
     // Handle if api domain is not available yet
@@ -18,4 +19,24 @@ async function fetchProperties() { // doing it like this because its a server-si
   }
 }
 
-export { fetchProperties };
+// Fetch single property
+async function fetchProperty(id) { // doing it like this because its a server-side component
+  try {
+    // Handle if api domain is not available yet
+    if (!apiDomain) {
+      return null // for deployment
+    }
+
+    const res = await fetch(`${apiDomain}/properties/${id}`) // need to include full uri, not just path because it' server-side
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    return res.json();
+  } catch (error) {
+    console.error(error);
+    return null; // for deployment
+  }
+}
+
+
+export { fetchProperties, fetchProperty };
